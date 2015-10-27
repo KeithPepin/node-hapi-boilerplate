6c13616768accb37977932733dc2b49fae79d55d
Keith Pepin <keith.pepin@gmail.com>
2015-10-27 00:21:23 -0400

    testing code-copter v1.3.1

---
+++ spec/quality_spec.js
@@ -0,0 +1,11 @@
+//'use strict';
+//var codeCopter = require('code-copter');
+//
+//console.log('testing with code-copter');
+//
+//codeCopter.configure({
+//    jscs: true,
+//    jshint: true
+//});
+//
+//describe('Code quality', codeCopter);
