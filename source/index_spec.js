a510809099b6b875674158b903dad9936e3a8aae
Keith Pepin <keith.pepin@gmail.com>
2015-10-28 00:19:39 -0400

    removed dummy tests

--- source/index_spec.js
+++ source/index_spec.js
@@ -1,14 +1,5 @@
 'use strict';
 
 describe('index_spec.js', function indexDescribe() {
-    beforeEach(function mainBeforeEach() {
-        spyOn(console, 'log').and.callThrough();
-    });
 
-    it('should call console.log() appropriately', function shouldCallConsoleLog() {
-        var testJS = require('./index.js');
-
-        expect(console.log.calls.count()).toBe(1);
-        expect(console.log).toHaveBeenCalledWith('testing 1-2-3');
-    });
 });
