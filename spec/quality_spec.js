4d79c6c712203808bfe4f3c31cb9febf5a74907b
Keith Pepin <keith.pepin@gmail.com>
2015-10-28 22:44:06 -0400

    Code cleanup to make code-copter happy

--- spec/quality_spec.js
+++ spec/quality_spec.js
@@ -1,10 +1,11 @@
 'use strict';
+
 var codeCopter = require('code-copter');
 
 codeCopter.configure({
+    exclude: ['coverage', 'node_modules'],
     jscs: true,
-    jshint: true,
-    exclude: ['coverage', 'node_modules', 'spec']
+    jshint: true
 });
 
 describe('Code quality', codeCopter);
